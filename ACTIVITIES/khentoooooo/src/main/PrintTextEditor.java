813a7725e00263f4c32ce227d6a711110ccff3e4
Khentoooo <khentfrancisco27@gmail.com>
2024-03-17 21:23:49 +0800

    LAB 2.3

---
+++ ACTIVITIES/khentoooooo/src/main/PrintTextEditor.java
@@ -0,0 +1,12 @@
+package main;
+
+public class PrintTextEditor extends TextEditor {
+
+    public PrintTextEditor(String text) {
+        super(text);
+    }
+
+    public void print() {
+        System.out.println(getText());
+    }
+}
